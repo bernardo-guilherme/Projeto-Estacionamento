61b367b79d9fbab82c8d161345da8d245c5102c9
BERNARDO GUILHERME FREIRE DE OLIVEIRA <bernardo.freire.708@ad.ufrn.br>
2026-04-13 14:04:44 -0300

    tratamento de erros

--- Estacionamento/src/main/java/org/example/Veiculo.java
+++ Estacionamento/src/main/java/org/example/Veiculo.java
@@ -1,17 +1,31 @@
 package org.example;
 import java.util.Scanner;
+import javax.swing.JOptionPane;
 
 public class Veiculo {
     private String placa;
     private String modelo;
 
     public Veiculo(){
-        Scanner entrada = new Scanner(System.in);
-        String placa, modelo;
-        System.out.println("Qual o modelo do seu veiculo?");
-        modelo = entrada.nextLine();
-        System.out.println("Qual a placa do seu veiculo?");
-        placa = entrada.nextLine();
+        boolean erro=false;
+        do {
+            Scanner entrada = new Scanner(System.in);
+            String placa, modelo;
+            System.out.println("Qual o modelo do seu veiculo?");
+            setModelo(entrada.nextLine());
+            System.out.println("Qual a placa do seu veiculo?");
+            setPlaca(entrada.nextLine());
+            if((this.modelo.isEmpty()) && (this.placa.isEmpty())){
+                JOptionPane.showMessageDialog(null,"É preciso cadastrar o modelo e a placa do veiculo.");
+                erro=true;
+            } else if ((!(this.modelo.isEmpty())) && (this.placa.isEmpty())) {
+                JOptionPane.showMessageDialog(null,"É preciso cadastrar a placa do veiculo.");
+                erro=true;
+            } else if ((!(this.modelo.isEmpty())) && (this.placa.isEmpty())) {
+                JOptionPane.showMessageDialog(null,"É preciso cadastrar o modelo do veiculo.");
+                erro=true;
+            }
+        }while(erro);   
     }
 
     public void setPlaca(){
