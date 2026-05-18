25fd589494a310f7d7ce2004bfb9b2c90388efb1
bernardo-guilherme <bernardo.bg2010fdo@gmail.com>
2026-05-18 14:20:26 -0300

    Update Ticket.java

--- Estacionamento/src/main/java/org/example/Ticket.java
+++ Estacionamento/src/main/java/org/example/Ticket.java
@@ -6,12 +6,17 @@ public class Ticket {
     private Veiculo carro;
     private LocalDateTime horaEntrada;
     private LocalDateTime horaSaida;
+    private double valorFinal;
 
     public void registrarSaida(){
         horaSaida = LocalDateTime.now();
         System.out.println("Saida resgistrada.");
     }
     public void calcularTempoEmHoras(){
-        System.out.printf("Tempo de permanencia: " + Duration.between(horaEntrada, horaSaida));
+        if(horaSaida = null){
+            System.out.printf("Nao eh possivel calcular o tempo de permanencia, pois nao ha hora de saida");
+        }else{
+            System.out.printf("Tempo de permanencia: " + Duration.between(horaEntrada, horaSaida));
+            valorFinal = calcualrValor(Duration.between(horaEntrada, horaSaida));
     }
 }
